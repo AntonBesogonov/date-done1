380918888fcf44ae8073ea0bb4b46387344c22a7
AntonBesogonov <anton_besogonov@bk.ru>
2023-03-13 16:55:51 +0500

    123

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -13,7 +13,7 @@ export function Map() {
       return obj.id === filtered;
    });
 
-   //условия счетчика.
+   //условия счетчика..
    useEffect(() => {
       let filtered = counter;
       if (filtered > 2 || filtered < 1) {
